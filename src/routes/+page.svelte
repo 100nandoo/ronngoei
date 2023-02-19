c3303abb1d471971b15d081f99abc5b7cb5ff86f
Fernando <100nandoo@gmail.com>
2023-02-19 21:06:40 +0800

    add tailwinds and daisy ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <a href="/post/demo-page">Link to the demo page</a>
+<p class="font-bold font-sans">bold</p>
+<button class="btn">Button</button>
 
 <img src="/images/demo-image.jpg@w=100+fm=png.png" alt="demo" width="100" height="100" />
