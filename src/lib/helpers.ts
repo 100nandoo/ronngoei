0cd8929e86a9528c8f0c24518d504afbd591d468
Fernando F. H <100nandoo@users.noreply.github.com>
2023-02-19 10:41:21 +0800

    Initial commit

---
+++ src/lib/helpers.ts
@@ -0,0 +1,58 @@
+import sharp from 'sharp';
+
+import type { Hast, Image } from '$lib/types';
+
+export const reduceHast = (hast: Hast): Hast => {
+	// remove all unnessesary elements from hast (like position, properties or newlines) so no unnecessary information gets built
+	return {
+		type: hast.type,
+		...(hast.children && {
+			children: hast.children
+				.filter((child: Hast) => !child.value || child.value !== '\n')
+				.map((child: Hast) => reduceHast(child))
+		}),
+		...(hast.value && { value: hast.value }),
+		...(hast.tagName && { tagName: hast.tagName }),
+		...(hast.properties && { properties: hast.properties })
+	};
+};
+
+export const addImagePropertiesToHast = async (hast: Hast): Promise<Hast> => {
+	if (hast.tagName === 'img' && hast.properties) {
+		let width = undefined;
+		let height = undefined;
+		try {
+			const metadata = await sharp(`src/${hast.properties.src}`).metadata();
+			width = metadata.width;
+			height = metadata.height;
+		} catch {
+			console.log(`[warning] could not open src/${hast.properties.src}`);
+		}
+
+		hast.properties = {
+			...(hast.properties ? hast.properties : {}),
+			width: width,
+			height: height
+		};
+	}
+	if (hast.children) {
+		hast.children = await Promise.all(
+			hast.children.map(async (child) => await addImagePropertiesToHast(child))
+		);
+	}
+	return hast;
+};
+
+export const getImageProperties = async (path: string): Promise<Image> => {
+	let width = undefined;
+	let height = undefined;
+	try {
+		const metadata = await sharp(`src/${path}`).metadata();
+		width = metadata.width;
+		height = metadata.height;
+	} catch {
+		console.log(`[warning] could not open src/${path}`);
+	}
+
+	return { src: path, width: width, height: height };
+};
---
+++ src/routes/images/[slug].[type]/+server.ts
@@ -0,0 +1,22 @@
+import type { RequestHandler } from './$types';
+import { error } from '@sveltejs/kit';
+import sharp from 'sharp';
+
+export const prerender = true;
+
+export const GET: RequestHandler = async ({ params }) => {
+	try {
+		const image = sharp(`src/images/${params.slug}.${params.type}`);
+
+		console.log(`[info] processing image src/images/${params.slug}.${params.type}`);
+
+		if (image) {
+			return new Response(await image.toBuffer(), {
+				headers: { 'Content-Type': `image/${(await image.metadata()).format}` }
+			});
+		}
+		throw error(500, 'image could not be opened properly');
+	} catch {
+		throw error(404, 'not found');
+	}
+};
