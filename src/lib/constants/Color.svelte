db20ce45e67152834f4dd25d291a68eb53d4ec4c
Fernando <100nandoo@gmail.com>
2023-02-20 11:24:09 +0800

    Add footer

---
+++ src/lib/constants/Color.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+  export const linkedin = '#0B65C2'
+  export const googleScholarColor = '#0B65C2'
+  export const orcidColor = '#A6CE39'
+  export const researchGateColor = '#00CCBB'
+  export const webOfScienceColor = '#000000'
+  export const primary = '#4B6BFB'
+</script>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte';
+	// import Header from '$lib/Header.svelte';
+	// import HeaderMobile from '$lib/HeaderMobile.svelte';
+	// import Sidebar from '$lib/Sidebar.svelte';
+	import { navigating } from '$app/stores';
+	import { Pulse } from 'svelte-loading-spinners';
+	import Color, { primary } from '../lib/constants/Color.svelte';
+	import '../app.css';
+</script>
+
+<!-- <Header /> -->
+<!-- <HeaderMobile /> -->
+<div class="grid-cols-1 flex flex-col-reverse xl:flex-row lg:grid-cols-3 xl:mx-28">
+	<div class="basis-1/3">
+		<!-- <Sidebar /> -->
+	</div>
+	<div class="basis-2/3">
+		{#if $navigating}
+			<div class="w-full place-items-center grid h-screen">
+				<Pulse size="60" color={primary} unit="px" duration="1s" />
+			</div>
+		{:else}
+			<slot />
+		{/if}
+	</div>
+</div>
+<Footer />
